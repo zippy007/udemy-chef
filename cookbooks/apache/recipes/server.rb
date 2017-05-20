3237e1a63efc4accd541fd1222c1d96cc6ad7597
Martin P <budapest.boy@upcmail.hu>
2017-05-20 14:18:59 +0000

    With template values

--- cookbooks/apache/recipes/server.rb
+++ cookbooks/apache/recipes/server.rb
@@ -2,13 +2,10 @@ package 'httpd' do
    action :install
 end
 
-file '/var/www/html/index.html' do
-    content "<html><head>Hello World</head><body>
-    <p>HOSTNAME: #{node['hostname']}</p>
-    <p>IP ADDRESS: #{node['ipaddress']}</p>
-    <p>CPU: #{node['cpu']['0']['mhz']}</p>
-    <p>MEMORY: #{node['memory']['total']}</p>
-    </body></html>"
+template '/var/www/html/index.html' do
+    source 'index.html.erb'
+    variables(
+       :name => 'Zippy Zip')
     action :create
     owner 'root'
     group 'root'
