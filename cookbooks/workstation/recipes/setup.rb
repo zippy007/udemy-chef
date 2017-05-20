e9e513feb8caac9b2dcf638401ad3cfa7dfa8e32
Martin P <budapest.boy@upcmail.hu>
2017-05-20 13:04:36 +0000

    Added ohoi variables

--- cookbooks/workstation/recipes/setup.rb
+++ cookbooks/workstation/recipes/setup.rb
@@ -4,8 +4,15 @@ end
 
 package 'ntp'
 
+node['ipaddress']
+node['memory']['total']
+
 file '/etc/motd' do
-    content 'This server is the property of ...'
+    content "This server is the property of Zippy
+    HOSTNAME: #{node['hostname']}
+    IP ADDRESS: #{node['ipaddress']}
+    CPU: #{node['cpu']['0']['mhz']}
+    MEMORY: #{node['memory']['total']}"
     action :create
     owner 'root'
     group 'root'
