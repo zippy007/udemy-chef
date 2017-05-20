753316bdf6b21a39e507a0447e69bbe614df4c48
Martin P <budapest.boy@upcmail.hu>
2017-05-20 13:17:40 +0000

    Added Ohio

--- cookbooks/apache/recipes/server.rb
+++ cookbooks/apache/recipes/server.rb
@@ -3,8 +3,15 @@ package 'httpd' do
 end
 
 file '/var/www/html/index.html' do
-    content '<html><head>Hello World</head><body><p>Hello World !</p></body></html>'
+    content "<html><head>Hello World</head><body>
+    <p>HOSTNAME: #{node['hostname']}</p>
+    <p>IP ADDRESS: #{node['ipaddress']}</p>
+    <p>CPU: #{node['cpu']['0']['mhz']}</p>
+    <p>MEMORY: #{node['memory']['total']}</p>
+    </body></html>"
     action :create
+    owner 'root'
+    group 'root'
 end
 
 service "httpd" do
